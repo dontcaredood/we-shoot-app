30bd42410ea40f19897edfb59aaee1085f567c13
dontcaredood <65106249+dontcaredood@users.noreply.github.com>
2024-07-30 16:31:38 +0530

    commit for adding history controller

--- src/main/java/com/weshoot/code/entity/OrderHistory.java
+++ src/main/java/com/weshoot/code/entity/OrderHistory.java
@@ -1,6 +1,8 @@
 package com.weshoot.code.entity;
 
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,15 +18,18 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Cacheable
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Transactional
 public class OrderHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "history_sequence")
-    @SequenceGenerator(name = "history_sequence", sequenceName = "history_sequence", initialValue= 1, allocationSize = 1)
+    @SequenceGenerator(name = "history_sequence", sequenceName = "history_sequence", initialValue = 1, allocationSize = 1)
     private long orderHistoryId;
     @Column(name = "orderId")
     private long orderId;
+    @Column(name = "action")
+    private String action;
+    @Column(name = "data",length = 500)
+    private String data;
     @Column(name = "timeStamp")
     private Date timeStamp;
     @Column(name = "userName")//change it to userid later
